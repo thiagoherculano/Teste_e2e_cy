09f88c6c2e18cf211b4e1332048adf7b319fa239
thiagoherculano <61213880+thiagoherculano@users.noreply.github.com>
2023-04-27 21:02:56 -0300

    Funcionalidade Pré Cadastro
    Feito a funcionalidade pré cadastro e implantada a funcionalidade Faker para criação de email e nome sobrenomes.

---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,21 @@
+var faker = require('@faker-js/faker');
+
+describe('Funcionalidade Pré cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    it('Deve completar o pré cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.faker.internet.email())
+        cy.get('#reg_password').type('!teste@teste$')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.faker.name.firstName())
+        cy.get('#account_last_name').type(faker.faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados')
+    });
+});
